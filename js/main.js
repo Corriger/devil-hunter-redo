e11e795d892fdbd1faf2f15b0f321ea54f91d9c4
Corriger <rogmoraldo2@gmail.com>
2017-08-17 00:42:57 -0400

    first commits

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $("header a").on('click',function(event){
+    event.preventDefault()
+    var display = $(this).attr("href")
+    $("#" + display).addClass("current")
+    $("article").not("#" + display).removeClass("current")
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var numsArray = [];
+// var total = 0;
+//
+// function beautiful(multiple1,multiple2,upwardBound){
+//   	for(var n=0;n<=upwardBound;n++){
+// 		if (n % multiple1 === 0 || n % multiple2 === 0){
+// 			numsArray.push(n);
+//   	}
+// 	}
+//   sum(numsArray);
+// }
+//
+// 	function sum(multiArr){
+// 		multiArr.forEach(function(a){
+//       total += a;
+//     });
+//    console.log(total);
+//   }
+//
+// beautiful(3,5,1000);
