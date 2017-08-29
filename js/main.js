70420fd4b27e2d2d38c9cbedef4dd3a0fb5f0ff2
Corriger <rogmoraldo2@gmail.com>
2017-08-29 18:57:43 -0400

    a bunch o stuff

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
-$(document).ready(function(){
-  $("header a").on('click',function(event){
-    var display = "#" + $(this).attr("href")
-        event.preventDefault();
-        $(display).addClass("current")
-        $("main section article").not(display).removeClass("current")
-        $(display).fadeIn("slow")
-  })
-  $("#tab-3 a").on('click',function(event){
-    event.preventDefault()
-    var style = "#" + $(this).attr("href")
-    $(style).addClass("current")
-    $("#tab-3 article").not(style).removeClass("current")
-    $(style).fadeIn("slow")
-  })
-  $("#tab-4 a").on('click',function(event){
-    event.preventDefault()
-    var gun = "#" + $(this).attr("href")
-    $(gun).addClass("current")
-    $("#tab-4 article").not(gun).removeClass("current")
-    $(gun).fadeIn("slow")
-  })
-  $("#tab-5 a").on('click',function(event){
-    event.preventDefault()
-    var dA = "#" + $(this).attr("href")
-    $(dA).addClass("current")
-    $("#tab-5 article").not(dA).removeClass("current")
-    $(dA).fadeIn("slow")
-  })
+$(document).ready(function() {
+    $("header a").on('click', function(event) {
+        var display = "#" + $(this).attr("href")
+        var seeShit = "rgba(5,1,0,.7)"
+        if(display != "#index-dt.html" && display != "#index.html"){
+          event.preventDefault();
+          $(display).addClass("current").css("background", seeShit)
+          $("main section article").not(display).removeClass("current")
+        }
+    })
+    $("#tab-3 a").on('click', function(event) {
+        event.preventDefault()
+        var style = "#" + $(this).attr("href")
+        $(style).addClass("current")
+        $("#tab-3 article").not(style).removeClass("current")
+        $(style).fadeIn("slow")
+    })
+    $("#tab-4 a").on('click', function(event) {
+        event.preventDefault()
+        var gun = "#" + $(this).attr("href")
+        $(gun).addClass("current")
+        $("#tab-4 article").not(gun).removeClass("current")
+        $(gun).fadeIn("slow")
+    })
+    $("#tab-5 a").on('click', function(event) {
+        var dA = "#" + $(this).attr("href")
+        var bell = "http://profiles.rphaven.com/The%20Legendary%20Devil%20Bane"
+        var bellLink = $(dA + "a").attr("href", bell)
+        if (dA != bellLink) {
+            event.preventDefault()
+            $(dA).addClass("current")
+            $("#tab-5 article").not(dA).removeClass("current")
+            $(dA).fadeIn("slow")
+        }
+    })
 })
 
 /*$("header a, #tab- a").on('click',function(event){
@@ -35,8 +41,6 @@ $(document).ready(function(){
   $(display).addClass("current")
   $("main section article").not(display).removeClass("current")
 })*/
-
-
 
 
 
